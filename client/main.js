c3d84e74dcf376771e072cf60a77ee49e85c7e2b
Frau-D <conny@gruseldusel.de>
2016-11-18 18:20:55 +0100

    tags as text in ui, tags deletable, prevent duplicates

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-import { Mongo } from 'meteor/mongo';
+import {Template} from 'meteor/templating';
+import {ReactiveVar} from 'meteor/reactive-var';
+import {Mongo} from 'meteor/mongo';
 
 export const Pieces = new Mongo.Collection('pieces');
 export const Tacks = new Mongo.Collection('tacks');
@@ -8,115 +8,149 @@ export const Tacks = new Mongo.Collection('tacks');
 import './main.html';
 
 var handleUpload = function (event, template) {
-  if (event.currentTarget.files && event.currentTarget.files[0]) {
+    if (event.currentTarget.files && event.currentTarget.files[0]) {
 
-    // We upload only one file, in case there were multiple files selected
-    var file = event.currentTarget.files[0];
-    if (file) {
-      var uploadInstance = Images.insert({
-        file: file,
-        streams: 'dynamic',
-        chunkSize: 'dynamic'
-      }, false);
+        // We upload only one file, in case there were multiple files selected
+        var file = event.currentTarget.files[0];
+        if (file) {
+            var uploadInstance = Images.insert({
+                file: file,
+                streams: 'dynamic',
+                chunkSize: 'dynamic'
+            }, false);
 
-      // uploadInstance.on('start', function() {template.currentUpload.set(this);});
+            // uploadInstance.on('start', function() {template.currentUpload.set(this);});
 
-      uploadInstance.on('end', function(error, fileObj) {
-        if (error) {
-          alert('Error during upload: ' + error.reason);
-        } else {
-          //alert('File "' + fileObj.name + '" successfully uploaded: '+fileObj._id);
-          template.data.image_ids.push(fileObj._id);
+            uploadInstance.on('end', function (error, fileObj) {
+                if (error) {
+                    alert('Error during upload: ' + error.reason);
+                } else {
+                    //alert('File "' + fileObj.name + '" successfully uploaded: '+fileObj._id);
+                    template.data.image_ids.push(fileObj._id);
 
-          Pieces.update(template.data._id, {
-            $set: {image_ids: template.data.image_ids}
-          });
+                    Pieces.update(template.data._id, {
+                        $set: {image_ids: template.data.image_ids}
+                    });
 
+                }
+                //template.currentUpload.set(false);
+            });
+
+            uploadInstance.start();
         }
-        //template.currentUpload.set(false);
-      });
-
-      uploadInstance.start();
     }
-  }
 };
 
+
+var getActualTagTexts = function () {
+
+};
+
+
 Template.uploadedFiles.helpers({
-  uploadedFiles: function () {
-    return Images.find(); // {}, {sort: { createdAt: -1 } } nach Uploaddatum sortieren...
-  }
-});
-
-
-Template.uploadForm.onCreated(function () {
-  this.currentUpload = new ReactiveVar(false);
+    uploadedFiles: function () {
+        return Images.find(); // {}, {sort: { createdAt: -1 } } nach Uploaddatum sortieren...
+    }
 });
 
 
 Template.uploadForm.helpers({
-  currentUpload: function () {
-    return Template.instance().currentUpload.get();
-  }
-});
-
-
-Template.uploadForm.events({
-  'change .fileInput': handleUpload
+    currentUpload: function () {
+        return Template.instance().currentUpload.get();
+    }
 });
 
 
 Template.body.helpers({
-  pieces() {
-    return Pieces.find({});
-  },
-  tack_id() {
-    return Tacks.findOne(tack_)[text];
-  }
+    pieces() {
+        return Pieces.find({});
+    }
 });
+
+
+Template.uploadForm.events({
+    'change .fileInput': handleUpload
+});
+
+
+Template.uploadForm.onCreated(function () {
+    this.currentUpload = new ReactiveVar(false);
+});
+
 
 // create new piece
 Template.body.events({
-  'submit .new-piece'(event) {
-    // Prevent default browser form submit
-    event.preventDefault();
+    'submit .new-piece'(event) {
+        // Prevent default browser form submit
+        event.preventDefault();
 
-    // Get value from form element
-    const target = event.target; // TODO: substitute this with the image_ids
+        // Get value from form element
+        const target = event.target; // TODO: substitute this with the image_ids
 
-    // Insert a piece into the collection
-    Pieces.insert({
-      text: event.target.text.value,
-      image_ids:[],
-      tag_ids: [],
-      user_id: 'Conny', // TODO: substitute this with actual user-id
-      createdAt: new Date()
-    });
+        // Insert a piece into the collection
+        Pieces.insert({
+            text: event.target.text.value,
+            image_ids: [],
+            tag_ids: [],
+            user_id: 'Conny', // TODO: substitute this with actual user-id
+            createdAt: new Date()
+        });
 
-    // Clear form
-    target.text.value = '';
-  }
+        // Clear form
+        target.text.value = '';
+    }
 });
 
 Template.piece.events({
-  'click .delete'() {
-    Pieces.remove(this._id); // removes piece-object from mongodb
-    // TODO: remove associated images
-  },
-  'change .fileInput'(event, template){
-    handleUpload(event, template);
-  },
-  'submit .new-tack'(event) {
-    event.preventDefault();
+    'click .delete'() {
+        Pieces.remove(this._id); // removes piece-object from mongodb
+        // TODO: remove associated images
+    },
+    'change .fileInput'(event, template){
+        handleUpload(event, template);
+    },
+    'submit .new-tack'(event) {
+        event.preventDefault();
 
-    const target = event.target;
-    var tag_id = Tacks.insert({
-      text: event.target.tacks.value,
-    });
-    this.tag_ids.push(tag_id);
-    Pieces.update(this._id, {
-      $set: {tag_ids: this.tag_ids}
-    });
-  }
+        const tag_text = event.target.tacks.value;
+        var tag_id;
+
+        var tag_from_db = Tacks.findOne({text: tag_text});
+        if (tag_from_db !== undefined) {
+            tag_id = tag_from_db._id
+        } else {
+            tag_id = Tacks.insert({text: tag_text});
+        }
+        if(this.tag_ids.indexOf(tag_id) == -1){ //prüfen ob's nicht drin
+            this.tag_ids.push(tag_id);
+            Pieces.update(this._id, {
+                $set: {tag_ids: this.tag_ids}
+            });
+        }
+        event.target.tacks.value = '';
+    }
 });
 
+Template.showTags.helpers({
+    tags() {
+        return Tacks.find({"_id": {"$in": this.tag_ids}});
+    }
+});
 
+Template.showTags.events({
+    'click .deleteTag'(event) {
+        console.log(this);
+        var deletedTag = event.target.dataset.tagId;
+        var tagIndex = this.tag_ids.indexOf(deletedTag);
+        this.tag_ids.splice(tagIndex, 1);
+        Pieces.update(this._id, {
+            $set: {tag_ids: this.tag_ids}
+        });
+    },
+});
+
+Template.showImages.helpers({
+    images() {
+        return Images.find({"_id": {"$in": this.image_ids}});
+    }
+});
