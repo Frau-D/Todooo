23465bac056fef289928cf8181cd166bd4f6b1b5
Frau-D <conny@gruseldusel.de>
2016-11-30 14:10:16 +0100

    reordered the code

--- client/main.js
+++ client/main.js
@@ -12,6 +12,115 @@ Meteor.startup(() => {
     Session.set('showOverview', true);
 });
 
+
+
+// BODY 
+
+Template.body.helpers({
+    showOverview() {
+        return Session.get('showOverview');
+    },
+    pieces() {
+        var piece_id = Session.get('clickedPieceId');
+        return Pieces.find({"_id": {"$in": [piece_id]}});
+    }
+
+});
+
+Template.body.events({
+    //create new piece
+    'submit .new-piece'(event) {
+        // Prevent default browser form submit
+        event.preventDefault();
+
+        // Insert a piece into the collection
+        var piece_id = Pieces.insert({
+            image_ids: [],
+            tag_ids: [],
+            user_id: 'Conny',
+            // TODO: substitute this with actual user-id
+            createdAt: new Date()
+        });
+        Session.set('clickedPieceId', piece_id);
+        Session.set('showOverview', false);
+    }
+});
+
+
+
+// OVERVIEW 
+
+Template.allClothes.helpers({
+    pieces() {
+        return Pieces.find(
+            {},
+            { sort: { createdAt: -1 } }
+        );
+    },
+    piece_id() {
+        return this._id;
+    },
+    first_image() {
+        return Images.find(
+            {"_id": {"$in": [this.image_ids[0]]}},
+        );
+    }
+});
+
+Template.allClothes.events({
+    'click .overviewImage'(event) {
+        piece_id = event.target.dataset.pieceid;
+        Session.set('clickedPieceId', piece_id);
+        Session.set('showOverview', false);
+    }
+});
+
+
+
+// OVERVIEW -> FILTER 
+
+var refresh_autocomplete = function(){
+    var availableTags = [];
+    Tacks.find({}).fetch().forEach(function(element, index,){
+        availableTags.push(element.text);
+    });
+    $( "#tags_autocomplete" ).autocomplete({
+        source: availableTags
+    });
+};
+
+Template.filterByTag.helpers({
+    allExistingTags() {         //TODO: remove as soon as filteredTags() works
+        refresh_autocomplete();
+        return Tacks.find({});
+    },
+    filteredTags() {
+        //refresh_autocomplete();
+    }
+});
+
+Template.filterByTag.events({
+    'submit .new-filter'(event) {
+        // Prevent default browser form submit
+        event.preventDefault();
+
+        const filter_text = event.target.filtered_tacks.value;
+        //this.filter_text.push(filter_text);
+        console.log(filter_text);
+        //Session.set('filters', filter_text);
+    }
+});
+
+Template.filterByTag.rendered = function(){
+    // refresh the autocompletion box when details page is closed
+    // and overview got rendered again
+    refresh_autocomplete();
+};
+
+
+
+// PIECE
+
 var handleUpload = function (event, template) {
     if (event.currentTarget.files && event.currentTarget.files[0]) {
 
@@ -46,106 +155,11 @@ var handleUpload = function (event, template) {
     }
 };
 
-var refresh_autocomplete = function(){
-    var availableTags = [];
-    Tacks.find({}).fetch().forEach(function(element, index){
-        availableTags.push(element.text);
-    });
-    $( "#tags_autocomplete" ).autocomplete({
-        source: availableTags
-    });
-};
-
-
-Template.filterByTag.rendered = function(){
-    // refresh the autocompletion box when details page is closed
-    // and overview got rendered again
-    refresh_autocomplete();
-};
-
-
-// HELPERS HELPERS HELPERS HELPERS HELPERS HELPERS HELPERS
-
-
-Template.filterByTag.helpers({
-    allExistingTags() {
-        refresh_autocomplete();
-        return Tacks.find({});
-    }
-});
-
-Template.body.helpers({
-    showOverview() {
-        return Session.get('showOverview');
-    },
-    pieces() {
-        var piece_id = Session.get('clickedPieceId');
-        return Pieces.find({"_id": {"$in": [piece_id]}});
-    }
-
-});
-
-Template.allClothes.helpers({
-    pieces() {
-        return Pieces.find(
-            {},
-            { sort: { createdAt: -1 } }
-        );
-    },
-    piece_id() {
-        return this._id;
-    },
-    first_image() {
-        return Images.find(
-            {"_id": {"$in": [this.image_ids[0]]}},
-        );
-    }
-});
-
-Template.showImages.helpers({
-    images() {
-        return Images.find(
-            {"_id": {"$in": this.image_ids}}
-        );
-    }
-});
-
-Template.showTags.helpers({
-    tags() {
-        return Tacks.find(
-            {"_id": {"$in": this.tag_ids}}
-        );
-    }
-});
-
-
-// EVENTS EVENTS EVENTS EVENTS EVENTS EVENTS EVENTS EVENTS 
-
-
-Template.body.events({
-    //create new piece
-    'submit .new-piece'(event) {
-        // Prevent default browser form submit
-        event.preventDefault();
-
-        // Insert a piece into the collection
-        var piece_id = Pieces.insert({
-            image_ids: [],
-            tag_ids: [],
-            user_id: 'Conny',
-            // TODO: substitute this with actual user-id
-            createdAt: new Date()
-        });
-        Session.set('clickedPieceId', piece_id);
-        Session.set('showOverview', false);
-    }
-});
-
 
 Template.piece.events({
     // remove piece-object from mongodb
     'click .delete'() {
-        Pieces.remove(this._id); 
+        Pieces.remove(this._id);
         // TODO: remove associated images
     },
     'change .fileInput'(event, template){
@@ -176,6 +190,41 @@ Template.piece.events({
 });
 
 
+Template.showImages.helpers({
+    images() {
+        return Images.find(
+            {"_id": {"$in": this.image_ids}}
+        );
+    }
+});
+
+
+// PIECE -> SHOW IMAGES
+
+Template.showImages.events({
+    'click .deleteImage'(event) {
+        //Images.remove(this._id);
+        var deletedImage = event.target.dataset.imageid;
+        var imageIndex = this.image_ids.indexOf(deletedImage);
+        this.image_ids.splice(imageIndex, 1); // deletes ONE element on position of imageIndex
+        Pieces.update(this._id, {
+            $set: {image_ids: this.image_ids}
+        });
+    }
+});
+
+
+// PIECE -> SHOW TAGS
+
+Template.showTags.helpers({
+    tags() {
+        return Tacks.find(
+            {"_id": {"$in": this.tag_ids}}
+        );
+    }
+});
+
+
 Template.showTags.events({
     'click .deleteTag'(event) {
         var deletedTag = event.target.dataset.tagId;
@@ -188,31 +237,11 @@ Template.showTags.events({
 });
 
 
-Template.allClothes.events({
-    'click .overviewImage'(event) {
-        piece_id = event.target.dataset.pieceid;
-        Session.set('clickedPieceId', piece_id);
-        Session.set('showOverview', false);
-    }
-});
-
+// PIECE -> OVERVIEW BUTTON
 
 Template.overviewButton.events({
     'click #overview'(event) {
         event.preventDefault();
         Session.set('showOverview', true);
-    }
-});
-
-
-Template.showImages.events({
-    'click .deleteImage'(event) {
-        //Images.remove(this._id);
-        var deletedImage = event.target.dataset.imageid;
-        var imageIndex = this.image_ids.indexOf(deletedImage);
-        this.image_ids.splice(imageIndex, 1); // deletes ONE element on position of imageIndex
-        Pieces.update(this._id, {
-            $set: {image_ids: this.image_ids}
-        });
     }
 });
